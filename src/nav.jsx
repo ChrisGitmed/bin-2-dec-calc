1078d1c8aa3090ef6da574fe4cd82e837109e76b
Chris Gitmed <cgitmed@gmail.com>
2021-01-29 06:05:11 +0000

    Added nav; Added basic styling.

---
+++ src/nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class NavBar extends React.Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <h1>Bin2Dec Calculator</h1>
+      </nav>
+    )
+  }
+}
