dee74983d150bc8746a591d1809ec6e29855ea64
Chris Gitmed <cgitmed@gmail.com>
2021-01-29 23:01:00 +0000

    Added a limit so no more than 8 characters can be input.

--- src/bin-2-dec-calc.jsx
+++ src/bin-2-dec-calc.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 
 export default class Bin2DecCalc extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      output: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    console.log('event.target.value: ', event.target.value);
+    this.setState({
+      input: event.target.value
+    })
+  }
+
   render() {
+    const { handleChange } = this;
+    const { input, output } = this.state;
+
     return (
       <div className="calc-frame">
         <header className="calc-header">
           <p>Enter up to 8 binary digits to view the decimal equivalent.</p>
         </header>
         <div className="calc-body">
-          <input type="text" placeholder="ex. 10100111"/>
+          <input type="text" placeholder="ex. 10100111" maxLength="8" onChange={handleChange}/>
+          <p className="converted-value">Output: {output}</p>
         </div>
       </div>
     )
