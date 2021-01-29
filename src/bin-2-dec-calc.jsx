30609260cfb7d8000e52c374554ecd595bdadf07
Chris Gitmed <cgitmed@gmail.com>
2021-01-29 08:10:43 +0000

    Added an input area for the binary input.

--- src/bin-2-dec-calc.jsx
+++ src/bin-2-dec-calc.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 export default class Bin2DecCalc extends React.Component {
   render() {
     return (
-      <h1>Hello world!</h1>
+      <div className="calc-frame">
+        <header className="calc-header">
+          <p>Enter up to 8 binary digits to view the decimal equivalent.</p>
+        </header>
+        <div className="calc-body">
+          <input type="text" placeholder="ex. 10100111"/>
+        </div>
+      </div>
     )
   }
 }
