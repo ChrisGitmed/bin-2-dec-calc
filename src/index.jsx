843a6473e80e3f0527fd27a232bc971c2a664c5e
Chris Gitmed <cgitmed@gmail.com>
2021-01-29 07:16:51 +0000

    Created the Bin2DecCalc component; Rendered it in index.js.

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from './nav.jsx'
+import Bin2DecCalc from './bin-2-dec-calc'
 
 ReactDOM.render(
-  <NavBar />,
+  <>
+    <NavBar />
+    <Bin2DecCalc />
+  </>,
   document.querySelector('#root')
 )
